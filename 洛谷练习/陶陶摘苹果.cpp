f7d1b15ec7b5a6c87aff6698e08c4d2eeb79ed26
lihuikong <3324567099@qq.com>
2025-11-24 22:54:12 +0800

    陶陶摘苹果

--- 洛谷练习/陶陶摘苹果.cpp
+++ 洛谷练习/陶陶摘苹果.cpp
@@ -2,4 +2,19 @@
 using namespace std;
 
 int main(){
+    int apple[10];
+    int hand_high;
+    for(int i=0;i<10;++i){
+        cin>>apple[i];
+    }
+    cin>>hand_high;
+    int total_height=hand_high+30;
+    int count=0;
+    for(int i=0;i<10;++i){
+        if(apple[i]<=total_height){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
 }
