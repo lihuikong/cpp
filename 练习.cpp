0ce5d0a37ab1b566160903bef89b82befa3e3d89
lihuikong <3324567099@qq.com>
2025-12-10 15:32:32 +0800

    update older

--- 练习.cpp
+++ 练习.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
-
 
 int main(){
     string s;
-    cin>>s;
+    getline(cin,s);
     cout<<s<<endl;
     return 0;
 }
