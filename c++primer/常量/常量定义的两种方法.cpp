d57c49eec150092a7a7cdd53c730efa54b86d6f1
lihuikong <3324567099@qq.com>
2025-12-14 13:25:36 +0800

    常量

---
+++ c++primer/常量/常量定义的两种方法.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#define Day 7
+
+int main(){
+    cout<<"一周有"<<Day<<"天"<<endl;
+    const int month=12;
+    cout<<"一年有"<<month<<"个月份"<<endl;
+    return 0;
+}
