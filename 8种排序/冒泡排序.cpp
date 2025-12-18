6713e86c4354fb0da07695e12508720238ffc0b5
lihuikong <3324567099@qq.com>
2025-12-18 10:38:38 +0800

    冒泡排序

---
+++ 8种排序/冒泡排序.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void bubblesort(vector<int>& arr){
+    int size=arr.size();
+    bool swapped=false;//为了防止外层循环重复循环（如果有一些数组是本来就排列好的，就没必要一直循环）。
+    for(int i=0;i!=size-1;++i){//这是循环的遍数。
+        for(int j=0;j!=size-1-i;++j){//这是每一层的循环
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);//交换两个数的顺序。
+                swapped=true;//如果遍历一遍发现是排列好的数组就更改swapped的真值。
+            }   
+        }
+        if(swapped==false){//如果有已经排列好的就直接退出。
+            break;
+        }
+    }
+}
+
+int main(){
+    srand(time(NULL));//随机生成数。
+    vector<int> arr;
+    cout<<"排列的原始数据:"<<endl;
+    for(int i=0;i!=10;++i){
+        arr.push_back(rand()%100);
+        cout<<arr.back()<<" ";//输出arr数组尾部的数。
+    }
+    cout<<endl;
+    bubblesort(arr);
+    cout<<"排列后的数据是："<<endl;
+    for(auto m:arr){
+        cout<<m<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
--- 练习.cpp
+++ 练习.cpp
@@ -1,14 +1,23 @@
 #include<iostream>
-#include<string>
-#include<vector>
 using namespace std;
-
-int main(){
-    short short_value=32767;
-    short_value+=1;
-    int m,n=1;
-    cout<<short_value<<endl;
-    return 0;
+/*函数 search（nums,target){
+对于i从0到nums长度-1：
+   对于j从0到nums长度-1：
+      if(nums【i】+nums【j】==target){
+      cout<<"["<<i<<","<<j<<"]"<<endl;
+      }
 }
-//short能表示的最大值为32767，当数据溢出时大多数不会报错而是输出一个不可预知的结果。
-//在这个程序中，符号为本来是0，因为溢出被改成了1就会变成一个负值。
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        for(int i=0;i!=n-1;++i){
+            for(int j=0;j!=n-1;++j){
+                if(nums[i]+nums[j]==target){
+                    cout<<"["<<i<<","<<j<<"]"<<endl;
+                }
+            }
+        }
+    }
+};
