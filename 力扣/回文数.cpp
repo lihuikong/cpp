663190418e5524669fbd7f730917a81367bacfea
lihuikong <3324567099@qq.com>
2025-12-23 17:39:12 +0800

    回文数

--- 力扣/回文数.cpp
+++ 力扣/回文数.cpp
@@ -1,3 +1,31 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+bool isPalindrome(int x)
+{
+    if( x < 0 || (x % 10 == 0 && x != 0))
+    {
+        return false;
+    }
+    int reversedHalf = 0;
+    while(x > reversedHalf)
+    {
+        reversedHalf = reversedHalf * 10 + x % 10;
+        x /= 10;
+    }
+    return x == reversedHalf || x == reversedHalf / 10;
+}
+int main()
+{
+    int x;
+    cin>>x;
+    if(isPalindrome(x))
+    {
+        cout<<"true"<<endl;
+    }else
+    {
+        cout<<"false"<<endl;
+    }
+    return 0;
+}
