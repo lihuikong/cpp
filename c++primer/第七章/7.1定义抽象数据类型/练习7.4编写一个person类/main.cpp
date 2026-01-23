5aea61009973ecbab1734d75808f056595b7fab5
lihuikong <3324567099@qq.com>
2026-01-23 19:47:54 +0800

    自动化makefile，运行make run

--- c++primer/第七章/7.1定义抽象数据类型/练习7.4编写一个person类/main.cpp
+++ c++primer/第七章/7.1定义抽象数据类型/练习7.4编写一个person类/main.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     //调用构造函数，在栈上创建一个名为
-    Person student("Jarvis","Henan University of Engineering");
+    Person student("Jarvis!!!","Henan University of Engineering");
     //调用已经在头文件public里面声明过的函数进行输出。
     student.printInfo();
     return 0;
