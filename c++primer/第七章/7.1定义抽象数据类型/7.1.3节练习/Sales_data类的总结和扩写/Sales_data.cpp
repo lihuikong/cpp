1d45def4d449a0d0cc2fb0c8b6e39d91c088b104
lihuikong <3324567099@qq.com>
2026-01-28 20:43:02 +0800

    feat:Sales_data类

---
+++ c++primer/第七章/7.1定义抽象数据类型/7.1.3节练习/Sales_data类的总结和扩写/Sales_data.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+struct Sales_data
+{
+    std::string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+// Sales_data的非成员接口函数
+Sales_data add(const Sales_data &, const Sales_data &);
+std::ostream &print(std::ostream &, const Sales_data &);
+std::istream &read(std::istream &, Sales_data &);
+
+double Sales_data::avg_price() const
+{
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+
+// 返回Sales_data类型的成员
+Sales_data &Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold; // 把rhs的成员加到this对象的成员上
+    revenue += rhs.revenue;
+    return *this;
+}
+
+std::istream &read(std::istream &is, Sales_data &item)
+{
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const Sales_data &item)
+{
+    os << item.isbn() << " " << item.units_sold << " "
+       << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs)
+{
+    Sales_data sum = lhs; // 拷贝lhs的数据成员给sum
+    sum.combine(rhs);     // 把rhs的数据成员加到sum中
+    return sum;
+}
